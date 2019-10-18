d48c457abd34dc074e0873e7bbbf8c9cf09b86e2
nuxavier <o.oaikh@yahoo.com>
2019-10-18 10:51:40 -0600

    changes made to repo

--- ClassLibrary1/CrmPackage/WebResources/Script (JScript)/AccountForm.js
+++
@@ -1,8 +0,0 @@
-﻿niAccountForm =
-{
-    onLoadEvents: function () {
-        //Xrm.Page.getControl("telephone1").addOnKeyPress(function () { niUtility.formatPhoneNumber("telephone1") });
-
-        //Xrm.Page.getControl("address1_postalcode").addOnKeyPress(function () { niUtility.formatPostalCode("address1_postalcode") });
-    }
-};
